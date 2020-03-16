3dfb5cd73108e99c82957a8b9d15677e7f842e21
Bruno Teixeira <bruno_teixeira@hotmail.com>
2020-03-16 12:31:28 -0300

    adicionando buscador de cep ao git

---
+++ script.js
@@ -0,0 +1,23 @@
+const cidade = document.querySelector('.cidade');
+const estado = document.querySelector('.estado');
+const ddd = document.querySelector('.ddd');
+const cepDigitado = document.querySelector('#cep');
+const botao = document.querySelector('.btn');
+
+botao.addEventListener('click', () => {
+  if (cepDigitado.value && !isNaN(cepDigitado.value)) {
+    axios.get(`https://cep.awesomeapi.com.br/${cepDigitado.value}`)
+      .then(function (response) {
+        cidade.innerText = `Cidade: ${response.data.city}`;
+        estado.innerText = `Estado: ${response.data.state}`;
+        ddd.innerText = `DDD: ${response.data.ddd}`;
+        console.log(response);
+      })
+      .catch(function (error) {
+        alert('Error, verifique os dados')
+        console.warn('Erro na requisição');
+      })
+  } else {
+    alert('Preencha o campo somente com numeros');
+  }
+});
